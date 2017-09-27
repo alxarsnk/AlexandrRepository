75e9db1c7e5001426ddf4295afba8363d91ddb64
Alex Arsenyuk <Alex Arsenyuk@DESKTOP-KEMRMSF>
2017-09-28 00:10:12 +0300

    Задача1-3

---
+++ zadacha1-3/zadacha1-3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semestrovka1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            if (Math.Abs(x2 - x1) == 1 && Math.Abs(y1 - y2) == 2 || Math.Abs(x2 - x1) == 2 && Math.Abs(y1 - y2) == 1)
+                Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+            Console.ReadKey();
+        }
+    }
+}
+
