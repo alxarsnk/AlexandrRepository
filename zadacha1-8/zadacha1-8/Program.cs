4d6d469c98f0f1c54dcabf41256b111be15c4f85
Alex Arsenyuk <Alex Arsenyuk@DESKTOP-KEMRMSF>
2017-09-28 00:08:49 +0300

    Задача1-8

---
+++ zadacha1-8/zadacha1-8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semestrovka1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double r1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double r2 = double.Parse(Console.ReadLine());
+            if (Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) <= (r1 + r2) && Math.Abs(r1 - r2) <= Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)))
+                Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+            Console.ReadKey();
+
+        }
+    }
+}
