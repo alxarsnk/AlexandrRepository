8598f339be1d25c40a4c996cce855321bc39c3a1
Alex Arsenyuk <Alex Arsenyuk@DESKTOP-KEMRMSF>
2017-09-28 08:39:52 +0300

    Zadacha2-1

---
+++ Zadacha2-1/Zadacha2-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int i,k=0;
+            int N = int.Parse(Console.ReadLine());
+            for (i = N-1; i > 0; i--)
+                k = k + i;
+            Console.WriteLine(k);
+            Console.ReadKey();
+
+        }
+    }
+}
