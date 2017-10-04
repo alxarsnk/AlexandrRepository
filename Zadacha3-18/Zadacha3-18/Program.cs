585643945e74a0490719f57ef52ab780fc1a4b9b
Alex Arsenyuk <Alex Arsenyuk@DESKTOP-KEMRMSF>
2017-10-04 10:08:50 +0300

    Zadacha4-4

---
+++ Zadacha3-18/Zadacha3-18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha3_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int i, x, n,j,d=0, b = 2;
+            n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int[] c = new int[n];
+            var array = Console.ReadLine().Split(' ');
+            for (i = 0; i < n; i++)
+            {
+                x = int.Parse(array[i]);
+                
+                    a[i] = x;
+                    for (j = 0; j < 100; j++)
+                        if (a[i] == Math.Pow(b, j))
+                        {
+                            c[d] = a[i];
+                            d++;
+                        }
+                
+            }
+
+            for (i = 0; i < d; i++)
+                Console.WriteLine(c[i]);
+
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ Zadacha4-4/Zadacha4-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == 5)
+                Console.WriteLine($"{x*x}");
+            else 
+            if (x % 5 == 0 && x % 10 != 0)
+            {
+                x = (x / 10 * x / 10 + 1) * 100 + 25;
+                Console.WriteLine($"{x}");
+            }
+            else Console.WriteLine("Ne podxodit");
+            Console.ReadKey();
+        }
+    }
+}
