a8325709054bfee81d7e263325edae24eae79ec5
Alex Arsenyuk <Alex Arsenyuk@DESKTOP-KEMRMSF>
2017-10-05 11:04:16 +0300

    5minutka(1)

---
+++ 5minutka-1/5minutka-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5minutka_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            if (y <= (x * x) && y >= x && x <= 0 && x >= -1 || x >= 0 && x <= 1 && y <= 0 && y >= -1)
+                Console.WriteLine("+");
+
+            else Console.WriteLine("-");
+            Console.ReadKey();
+        }
+    }
+}
